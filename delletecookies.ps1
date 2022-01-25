23abea63761027368f9e83fa863a0e7b30f58f44
mrrobots101 <98418821+mrrobots101@users.noreply.github.com>
2022-01-26 01:13:29 +0700

    Create delletecookies.ps1

---
+++ delletecookies.ps1
@@ -0,0 +1,10 @@
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\Extension Cookies" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\Cache" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\Login Data" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\Login Data For Account" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\Shortcuts" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\Visited Links" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\History" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\" -Force -Recurse
+Remove-Item -Path "${env:LOCALAPPDATA}\Google\Chrome\User Data\Default\" -Force -Recurse
+
